8d816a610e34c8f6029e86fbcd313499fa7c4639
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-06-29 16:42:04 +0300

    1

--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -1,3 +1,30 @@
-export const ForbesList = () => {
-  return <h2>ForbesList</h2>;
+import { ForbesListItem } from 'components/ForbesListItem/ForbesListItem';
+import style from './ForbesList.module.css';
+
+export const ForbesList = ({ list = [] }) => {
+  return (
+    <div className={style.board}>
+      <div className={style.header}>
+        <h2 className={style.title}>
+          <span className={style.titleTop}>Forbes</span>
+          <span className={style.titleBottom}>Leader board</span>
+        </h2>
+      </div>
+
+      <ul className={style.list}>
+        {list.map(obj => {
+          return (
+            <li className={style.item} key={obj.id}>
+              <ForbesListItem
+                avatar={obj.avatar}
+                capital={obj.capital}
+                isIncrease={obj.isIncrease}
+                name={obj.name}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
